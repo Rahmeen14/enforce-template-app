e1b9756dda43f4f6a4501794ac9e5421473b69cd
rahmeen <rahmeenwill99@gmail.com>
2018-08-29 00:34:03 +0530

    Implementation for pull requests

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const {
   fetchJSON,
   getOwner,
   getRepo,
-  getCommentOnSuccess,
-  getCommentOnFailure,
-  isIssueFollowingTemplate,
+  getIssueCommentOnSuccess,
+  getIssueCommentOnFailure,
+  getPullRequestCommentOnSuccess,
+  getPullRequestCommentOnFailure,
+  isFollowingTemplate,
   getTemplateFields
 } = require('./util')
 /**
@@ -17,8 +19,8 @@ module.exports = app => {
 
   app.on('issues.opened', async context => {
     const issueBody = (context.payload.issue.body)
-    const issueCommentOnSuccess = getCommentOnSuccess(context)
-    const issueCommentOnFailure = getCommentOnFailure(context)
+    const issueCommentOnSuccess = getIssueCommentOnSuccess(context)
+    const issueCommentOnFailure = getIssueCommentOnFailure(context)
     const owner = getOwner(context)
     const repo = getRepo(context)
     /**
@@ -39,22 +41,60 @@ module.exports = app => {
     /**
      * A boolean to flag the violation of issue template
      */
-    let isFollowingTemplate = true
+    let isIssueFollowingTemplate = true
     /**
      * Checks for occurance of every template field in the
      * issue body and returns an appropriate comment based
      * on the flag value
      */
     templateFields.forEach(function (templateField) {
-      if (!isIssueFollowingTemplate(templateField, issueBody)) {
-        isFollowingTemplate = false
+      if (!isFollowingTemplate(templateField, issueBody)) {
+        isIssueFollowingTemplate = false
       }
     })
-    return isFollowingTemplate
+    return isIssueFollowingTemplate
     ? context.github.issues.createComment(issueCommentOnSuccess)
     : context.github.issues.createComment(issueCommentOnFailure)
   })
-
+  app.on('pull_request.opened', async context => {
+    const pullRequestBody = (context.payload.pull_request.body)
+    const pullRequestCommentOnSuccess = getPullRequestCommentOnSuccess(context)
+    const pullRequestCommentOnFailure = getPullRequestCommentOnFailure(context)
+    const owner = getOwner(context)
+    const repo = getRepo(context)
+    /**
+    * Fetch contents of ISSUE_TEMPLATE.md containing
+    * content in base64 format
+    */
+    const templateBody = await fetchJSON(
+      `https://api.github.com/repos/${owner}/${repo}/contents/PULL_REQUEST_TEMPLATE.md`
+    )
+   /**
+    * Parse the fetched JSON issue template and
+    * extract all fields from it for comparasion
+    * against the issue body
+    */
+    let encoded = JSON.parse(templateBody)
+    let templateBytes = base64.decode(encoded.content)
+    const templateFields = getTemplateFields(templateBytes)
+    /**
+     * A boolean to flag the violation of issue template
+     */
+    let isPullRequestFollowingTemplate = true
+    /**
+     * Checks for occurance of every template field in the
+     * issue body and returns an appropriate comment based
+     * on the flag value
+     */
+    templateFields.forEach(function (templateField) {
+      if (!isFollowingTemplate(templateField, pullRequestBody)) {
+        isPullRequestFollowingTemplate = false
+      }
+    })
+    return isPullRequestFollowingTemplate
+    ? context.github.issues.createComment(pullRequestCommentOnSuccess)
+    : context.github.issues.createComment(pullRequestCommentOnFailure)
+  })
   // For more information on building apps:
   // https://probot.github.io/docs/
 
--- util.js
+++ util.js
@@ -42,17 +42,37 @@ const getCommentOnFailure = context => context.issue({ body: 'Please abide by th
  * @return {boolean} whether issue body contains the given templateField
  *
  */
-const isIssueFollowingTemplate = (templateField, issueBody) => {
-  if (issueBody.includes(templateField)) {
+/**
+ * @param {Context} context
+ * @return {object} isssue comment body
+ *
+ */
+const getPullRequestCommentOnSuccess = context => context.issue({ body: 'Thank you for your pull request' })
+
+/**
+ * @param {Context} context
+ * @return {object} issue comment body
+ *
+ */
+const getPullRequestCommentOnFailure = context => context.issue({ body: 'Please abide by the template PULL_REQUEST_TEMPLATE.md while raising PRs' })
+
+/**
+ * @param {string} templateField
+ * @param {object} issueBody
+ * @return {boolean} whether issue body contains the given templateField
+ *
+ */
+const isFollowingTemplate = (templateField, bodyText) => {
+  if (bodyText.includes(templateField)) {
     return true
   }
   // Handles radio button markdown
   return (templateField.includes('* [ ]')
-  ? [templateField, templateField.replace('* [ ]', '* [x]')].some(l => issueBody.includes(l))
-  : issueBody.includes(templateField)) ||
+  ? [templateField, templateField.replace('* [ ]', '* [x]')].some(l => bodyText.includes(l))
+  : bodyText.includes(templateField)) ||
   (templateField.includes('- [ ]')
-  ? [templateField, templateField.replace('- [ ]', '- [x]')].some(l => issueBody.includes(l))
-  : issueBody.includes(templateField))
+  ? [templateField, templateField.replace('- [ ]', '- [x]')].some(l => bodyText.includes(l))
+  : bodyText.includes(templateField))
 }
 
 /**
@@ -63,7 +83,9 @@ const isIssueFollowingTemplate = (templateField, issueBody) => {
 const getTemplateFields = templateBytes => templateBytes.toString().split('\n')
 
 exports.getTemplateFields = getTemplateFields
-exports.isIssueFollowingTemplate = isIssueFollowingTemplate
+exports.getPullRequestCommentOnFailure = getPullRequestCommentOnFailure
+exports.getPullRequestCommentOnSuccess = getPullRequestCommentOnSuccess
+exports.isFollowingTemplate = isFollowingTemplate
 exports.getCommentOnSuccess = getCommentOnSuccess
 exports.getCommentOnFailure = getCommentOnFailure
 exports.fetchJSON = fetchJSON
