2aecaa639a7717f980782485fddcaad63f29f36d
Rahmeen Habib <rahmeenwill99@gmail.com>
2018-09-10 11:51:09 +0530

    Added implementation for the bot. (#2)
    * Checks if template is abided by and comments accordingly
    
    * Minor change to code
    
    * Handling radio buttons as well
    
    * Basic functionality implemented'
    
    * Added tests for isIssueFollowingTemplates
    
    * Added tests for index.js
    
    * Updated readme.md
    
    * Implementation for pull requests
    
    * Refactored code
    
    * Improved refactoring
    
    * Fixed typos
    
    * Update README.md
    
    * Addressed comments on PR
    
    * Update README.md
    
    * Update package.json
    
    * Prettified files
    
    * Prettified test files

--- index.js
+++ index.js
@@ -1,40 +1,108 @@
 const {
   fetchJSON,
   getOwner,
-  getRepo
+  getRepo,
+  getComment,
+  isBodyFollowingTemplate,
+  getFinalComment,
+  parseBody,
+  ISSUE_TEMPLATE_FAILURE_MESSAGE,
+  ISSUE_TEMPLATE_SUCCESS_MESSAGE,
+  PULL_REQUEST_TEMPLATE_SUCCESS_MESSAGE,
+  PULL_REQUEST_TEMPLATE_FAILURE_MESSAGE,
 } = require('./util')
 /**
  * This is the entry point for your Probot App.
  * @param {import('probot').Application} app - Probot's Application class.
  */
-var base64 = require('base-64')
 module.exports = app => {
-  // Your code here
   app.log('Yay, the app was loaded!')
 
   app.on('issues.opened', async context => {
-    const issueBody = (context.payload.issue.body)
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    const issueBody = context.payload.issue.body
+    const issueCommentOnSuccess = getComment(
+      context,
+      ISSUE_TEMPLATE_SUCCESS_MESSAGE
+    )
+    const issueCommentOnFailure = getComment(
+      context,
+      ISSUE_TEMPLATE_FAILURE_MESSAGE
+    )
     const owner = getOwner(context)
     const repo = getRepo(context)
     /**
-    * Fetch contents of readme.md containing
-    * content in base64 format
-    */
+     * Fetch contents of ISSUE_TEMPLATE.md containing
+     * content in base64 format
+     */
     const templateBody = await fetchJSON(
-      `https://api.github.com/repos/${owner}/${repo}/contents/README.md`
+      `https://api.github.com/repos/${owner}/${repo}/contents/ISSUE_TEMPLATE.md`
     )
-    context.log('issueBody', issueBody)
-    var encoded = JSON.parse(templateBody)
-    var bytes = base64.decode(encoded.content)
-    console.log('templateBody', bytes)
-
-    /* if (issueBody === templateBody) {
-      context.log(context.payload.issue.body)
-    } */
-    return context.github.issues.createComment(issueComment)
+    /**
+     * Parse the fetched JSON issue template and
+     * extract all fields from it for comparasion
+     * against the issue body
+     */
+    let templateFields = []
+    try {
+      templateFields = parseBody(templateBody)
+    } catch (err) {
+      console.error(err)
+    }
+    /**
+     * Checks for occurance of every template field in the
+     * issue body and returns an appropriate comment based
+     * on whether the template is abided by
+     */
+    return getFinalComment(
+      context,
+      isBodyFollowingTemplate(templateFields, true, issueBody),
+      issueCommentOnSuccess,
+      issueCommentOnFailure
+    )
   })
 
+  app.on('pull_request.opened', async context => {
+    const pullRequestBody = context.payload.pull_request.body
+    const pullRequestCommentOnSuccess = getComment(
+      context,
+      PULL_REQUEST_TEMPLATE_SUCCESS_MESSAGE
+    )
+    const pullRequestCommentOnFailure = getComment(
+      context,
+      PULL_REQUEST_TEMPLATE_FAILURE_MESSAGE
+    )
+    const owner = getOwner(context)
+    const repo = getRepo(context)
+    /**
+     * Fetch contents of PULL_REQUEST_TEMPLATE.md containing
+     * content in base64 format
+     */
+    const templateBody = await fetchJSON(
+      `https://api.github.com/repos/${owner}/${repo}/contents/PULL_REQUEST_TEMPLATE.md`
+    )
+    /**
+     * Parse the fetched JSON pull request template and
+     * extract all fields from it for comparasion
+     * against the PR body
+     */
+    let templateFields = []
+    try {
+      templateFields = parseBody(templateBody)
+    } catch (err) {
+      console.error(err)
+    }
+    /**
+     * Checks for occurance of every template field in the
+     * PR body and returns an appropriate comment based
+     * on whether the template is abided by
+     */
+    return getFinalComment(
+      context,
+      isBodyFollowingTemplate(templateFields, true, pullRequestBody),
+      pullRequestCommentOnSuccess,
+      pullRequestCommentOnFailure
+    )
+  })
   // For more information on building apps:
   // https://probot.github.io/docs/
 
--- util.js
+++ util.js
@@ -1,5 +1,11 @@
 const fetch = require('node-fetch')
-
+var base64 = require('base-64')
+const ISSUE_TEMPLATE_SUCCESS_MESSAGE = 'Thank you for opening this issue'
+const ISSUE_TEMPLATE_FAILURE_MESSAGE =
+  'Please abide by the template ISSUE_TEMPLATE.md while opening issues'
+const PULL_REQUEST_TEMPLATE_SUCCESS_MESSAGE = 'Thank you for the pull request'
+const PULL_REQUEST_TEMPLATE_FAILURE_MESSAGE =
+  'Please abide by the template PULL_REQUEST_TEMPLATE.md while raising PRs'
 /**
  * @param {string} url
  * @param {object} [options] as accepted by `fetch`
@@ -16,6 +22,14 @@ const fetchJSON = (url, options = {}) =>
 const getOwner = context => context.payload.repository.owner.login
 
 /**
+ * @param {string} templateBody
+ * @return {Array} an array of template fields
+ */
+const parseBody = templateBody => {
+  let encoded = JSON.parse(templateBody)
+  return getTemplateFields(base64.decode(encoded.content))
+}
+/**
  * @param {Context} context
  * @return {string} repo name
  *
@@ -24,10 +38,84 @@ const getRepo = context => context.payload.repository.name
 
 /**
  * @param {Context} context
- * @return {string} userName of the commenter
+ * @param {string} message
+ * @return {object} comment body
  *
  */
+const getComment = (context, message) => context.issue({ body: message })
 
+/**
+ * @param {string} templateField
+ * @param {object} bodyText
+ * @return {boolean} whether issue/PR body contains the given templateField
+ *
+ */
+const isFollowingTemplate = (templateField, bodyText) => {
+  if (bodyText.includes(templateField)) {
+    return true
+  }
+  // Handles radio button markdown
+  return (
+    (templateField.includes('* [ ]')
+      ? [templateField, templateField.replace('* [ ]', '* [x]')].some(l =>
+          bodyText.includes(l)
+        )
+      : bodyText.includes(templateField)) ||
+    (templateField.includes('- [ ]')
+      ? [templateField, templateField.replace('- [ ]', '- [x]')].some(l =>
+          bodyText.includes(l)
+        )
+      : bodyText.includes(templateField))
+  )
+}
+
+/**
+ * @param {base64} templateBytes
+ * @return {Array} string array of template fields
+ *
+ */
+const getTemplateFields = templateBytes => templateBytes.toString().split('\n')
+
+/**
+ * @param {Array} templateFields
+ * @param {boolean} followsTemplateBoolean
+ * @param {object} bodyText
+ * @return {boolean} whether the issue/PR body follow the template
+ */
+const isBodyFollowingTemplate = (
+  templateFields,
+  followsTemplateBoolean,
+  bodyText
+) =>
+  templateFields.every(t => isFollowingTemplate(t, bodyText)) ||
+  followsTemplateBoolean
+/**
+ * @param {Context} context
+ * @param {boolean} isFollowingTemplate
+ * @param {object} commentOnSuccess
+ * @param {object} commentOnFailure
+ * @return {object} final comment to be displayed eventually
+ *
+ */
+const getFinalComment = (
+  context,
+  isFollowingTemplate,
+  commentOnSuccess,
+  commentOnFailure
+) =>
+  context.github.issues.createComment(
+    isFollowingTemplate ? commentOnSuccess : commentOnFailure
+  )
+exports.ISSUE_TEMPLATE_FAILURE_MESSAGE = ISSUE_TEMPLATE_FAILURE_MESSAGE
+exports.ISSUE_TEMPLATE_SUCCESS_MESSAGE = ISSUE_TEMPLATE_SUCCESS_MESSAGE
+exports.PULL_REQUEST_TEMPLATE_FAILURE_MESSAGE = PULL_REQUEST_TEMPLATE_FAILURE_MESSAGE
+exports.PULL_REQUEST_TEMPLATE_SUCCESS_MESSAGE = PULL_REQUEST_TEMPLATE_SUCCESS_MESSAGE
+exports.getTemplateFields = getTemplateFields
+exports.getFinalComment = getFinalComment
+exports.isFollowingTemplate = isFollowingTemplate
+exports.isBodyFollowingTemplate = isBodyFollowingTemplate
+exports.getComment = getComment
 exports.fetchJSON = fetchJSON
 exports.getOwner = getOwner
 exports.getRepo = getRepo
+exports.parseBody = parseBody
